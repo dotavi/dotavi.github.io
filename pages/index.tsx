cff4e794727e89ed73d674a7ea16a348270071ec
Avinash Sewpersadh <avi.ukzn@gmail.com>
2023-04-12 23:54:31 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -11,11 +12,20 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <div className={styles.profileImage}>
+          <Image
+            src="/your_profile_picture.jpg"
+            alt="Avinash Sewpersadh"
+            width={150}
+            height={150}
+            className={styles.rounded}
+          />
+        </div>
         <h1 className={styles.title}>
           Hi, I&apos;m Avinash Sewpersadh
         </h1>
         <h2 className={styles.subtitle}>
-          
+          Engineer
         </h2>
 
         <p className={styles.description}>
@@ -23,9 +33,9 @@ export default function Home() {
         </p>
 
         <div className={styles.links}>
-          <a href="https://github.com/dotavi" target="_blank" rel="noopener noreferrer">GitHub</a>
-          <a href="https://linkedin.com/in/asewpersadh" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          <a href="mailto:avi.ukzn@gmail.com">Email</a>
+          <a href="https://github.com/dotavi" target="_blank" rel="noopener noreferrer" className={styles.link}>GitHub</a>
+          <a href="https://linkedin.com/in/asewpersadh" target="_blank" rel="noopener noreferrer" className={styles.link}>LinkedIn</a>
+          <a href="mailto:avi.ukzn@gmail.com" className={styles.link}>Email</a>
         </div>
       </main>
 
