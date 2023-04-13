094b1b0283bbffba7aa6d4a31e7184c36ed9516c
Avinash Sewpersadh <avi.ukzn@gmail.com>
2023-04-13 09:39:39 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${darkMode ? styles.dark : ''}`}>
       <Head>
         <title>Avinash Sewpersadh</title>
         <meta name="description" content="Avinash Sewpersadh - Portfolio" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <main className={styles.main}>
