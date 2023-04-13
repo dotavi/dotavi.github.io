770689eb8db1926c99dac116568646949b561c4b
Avinash Sewpersadh <avi.ukzn@gmail.com>
2023-04-13 14:20:27 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
   return (
-    <div className={`${styles.container} ${darkMode ? styles.dark : ''}`}>
+    <div className={styles.container}>
       <Head>
         <title>Avinash Sewpersadh</title>
         <meta name="description" content="Avinash Sewpersadh - Portfolio" />
@@ -25,18 +18,14 @@ export default function Home() {
         <h1 className={styles.title}>
           Hi, I&apos;m Avinash Sewpersadh
         </h1>
-        <h2 className={styles.subtitle}>
-   
-        </h2>
-
         <p className={styles.description}>
-         an engineer with a passion for creating innovative solutions.
+          an engineer with a passion for creating innovative solutions.
         </p>
 
         <div className={styles.links}>
-          <a href="https://github.com/dotavi" target="_blank" rel="noopener noreferrer" className={styles.link}>GitHub</a>
-          <a href="https://linkedin.com/in/asewpersadh" target="_blank" rel="noopener noreferrer" className={styles.link}>LinkedIn</a>
-          <a href="mailto:avi.ukzn@gmail.com" className={styles.link}>Email</a>
+          <a href="https://github.com/dotavi" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://linkedin.com/in/asewpersadh" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="mailto:avi.ukzn@gmail.com">Email</a>
         </div>
       </main>
 
